837f2335dd8b9a117fbd7f3f40d08c66c7150eed
romalxr <romalxr@gmail.com>
2023-02-21 05:45:27 +0300

    Add auth logs

--- Account Service/task/src/account/AccountServiceApplication.java
+++ Account Service/task/src/account/AccountServiceApplication.java
@@ -1,6 +1,5 @@
 package account;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class AccountServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
-
 }
